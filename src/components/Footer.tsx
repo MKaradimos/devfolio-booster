60356ad88fadc4a430f709c714eebd80652a1465
MKARADIMOS <karadimosmixalis@gmail.com>
2025-10-20 12:52:44 +0300

    change footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,10 @@ const Footer = () => {
   return (
     <footer className="bg-foreground text-background py-8">
       <div className="container mx-auto px-4 text-center">
-        <p className="text-sm">
-          © {new Date().getFullYear()}
+      <p className="text-sm text-gray-400 mt-4 sm:mt-0">
+          © {new Date().getFullYear()} Michail Karadimos — All Rights Reserved
         </p>
+      
       </div>
     </footer>
   );
