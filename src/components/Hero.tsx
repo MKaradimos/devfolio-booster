e979e3b203a5bb93ae461862332812deeb5e9d8b
MKARADIMOS <karadimosmixalis@gmail.com>
2025-10-20 12:42:53 +0300

    Test Netlify auto deploy

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -61,7 +61,7 @@ const Hero = () => {
                 onClick={scrollToContact}
               >
                 <Mail className="h-5 w-5" />
-                Contact Meeeeeeee
+                Contact Meeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
               </Button>
             </div>
           </div>
