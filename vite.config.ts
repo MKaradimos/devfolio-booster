6a8aff980a420ff21fdfdcc712c171bed104b6fa
MKARADIMOS <karadimosmixalis@gmail.com>
2025-10-20 12:48:18 +0300

    Fix Vite base path for Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./", // 👈 ΠΡΟΣΘΗΚΗ για σωστά paths στο Netlify
   server: {
     host: "::",
     port: 8080,
